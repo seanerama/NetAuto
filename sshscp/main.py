23fb490781f2b4aea61a2e56a8adf8a9814a678b
Sean Mahoney <sdmahoney01@gmail.com>
2024-07-30 09:49:05 -0500

    updated requirements

--- sshscp/main.py
+++ sshscp/main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
+import yaml
 
+
+# Load the config file
+with open("config.yaml", "r") as file:
+    config = yaml.safe_load(file)
 app = FastAPI()
+
+usr = config['network_devices']['cli_username']
+cli_pw = config['network_devices']['cli_password']
+
 
 @app.get("/status")
 async def get_status():
